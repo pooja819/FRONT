d05e49259729a899d6af4481713b1005d01eb82e
pooja819 <102895749+pooja819@users.noreply.github.com>
2024-02-10 19:53:06 +0530

    Add files via upload

---
+++ src/comp/customer/categories.js
@@ -0,0 +1,39 @@
+const categories = [
+  {
+    category_id: 101,
+    category_name: "Electronics",
+  },
+  {
+    category_id: 102,
+    category_name: "Gadgets",
+  },
+  {
+    category_id: 103,
+    category_name: "Audio",
+  },
+  {
+    category_id: 104,
+    category_name: "Gaming",
+  },
+  {
+    category_id: 105,
+    category_name: "Fitness",
+  },
+  {
+    category_id: 106,
+    category_name: "Furniture",
+  },
+  {
+    category_id: 107,
+    category_name: "Kitchen Appliances",
+  },
+  {
+    category_id: 108,
+    category_name: "Fashion",
+  },
+  {
+    category_id: 109,
+    category_name: "Art & Design",
+  },
+];
+export default categories;
